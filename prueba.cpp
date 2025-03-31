69cccf9e082e03ed15e9ce0a9c9e07651ef21894
DiegoMF964 <diegomf6@hotmail.com>
2025-03-30 23:10:50 -0500

    Update prueba.cpp

--- prueba.cpp
+++ prueba.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
-using namespace std;
-int main() {
-    int num1, num2, suma;
-
-    cout << "Ingrese el primer número: ";
-    cin >> num1;
-
-   cout << "Ingrese el segundo número: ";
-    cin >> num2;
-
-    suma = num1 + num2;
-
-    cout << "La suma de " << num1 << " y " << num2 << " es: " << suma << std::endl;
+#include <conio.h>
+using namespace std; 
+int main()
+{
+    const float pi=3.14;
+    int altura, radio;
+    float area, volumen;
+    
+    cout<<"Indique el radio: "<<endl;
+    cin>>altura;
+    cout<<"Indique la altura: "<<endl; 
+    cin>>radio;
+    
+    area = 2*pi*radio*(radio+altura);
+    volumen = pi*radio*radio*altura; 
+    cout<<"El area superficial del cilindro es: "<<area<<endl; 
+    cout<<"El volumen del cilindro es: "<<volumen; 
+    
+    
+    getch(); 
 
     return 0;
 }
