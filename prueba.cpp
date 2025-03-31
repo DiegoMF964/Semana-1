c03d51cd789df46d5b2ee4abfb4b63176255b3f6
DiegoMF964 <diegomf6@hotmail.com>
2025-03-30 22:50:57 -0500

    subida de archivo

---
+++ prueba.cpp
@@ -0,0 +1,11 @@
+    #include <iostream>
+    using namespace std;
+
+    int main() {
+        int numero;
+
+        cout <<"Indique su edad: ";
+        cin>>numero;
+        return 0;
+    }
+    
