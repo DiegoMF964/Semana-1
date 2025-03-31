6d86efdd71aa2cfc8c3455814cfefd7772d68644
DiegoMF964 <diegomf6@hotmail.com>
2025-03-30 23:08:17 -0500

    Mathias

--- prueba.cpp
+++ prueba.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     int num1, num2, suma;
 
-    std::cout << "Ingrese el primer número: ";
-    std::cin >> num1;
+    cout << "Ingrese el primer número: ";
+    cin >> num1;
 
-    std::cout << "Ingrese el segundo número: ";
-    std::cin >> num2;
+   cout << "Ingrese el segundo número: ";
+    cin >> num2;
 
     suma = num1 + num2;
 
-    std::cout << "La suma de " << num1 << " y " << num2 << " es: " << suma << std::endl;
+    cout << "La suma de " << num1 << " y " << num2 << " es: " << suma << std::endl;
 
     return 0;
 }
