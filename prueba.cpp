7ca336fc296e98d4cee32058d11f63fd54b9c203
DiegoMF964 <diegomf6@hotmail.com>
2025-03-30 22:56:12 -0500

    subida de archivo

--- prueba.cpp
+++ prueba.cpp
@@ -1,11 +1,24 @@
-    #include <iostream>
-    using namespace std;
-
-    int main() {
-        int numero;
-
-        cout <<"Indique su edad: ";
-        cin>>numero;
-        return 0;
-    }
+#include <iostream>
+#include <conio.h>
+using namespace std; 
+int main()
+{
+    const float pi=3.14;
+    int altura, radio;
+    float area, volumen;
     
+    cout<<"Indique el radio: "<<endl;
+    cin>>altura;
+    cout<<"Indique la altura: "<<endl; 
+    cin>>radio;
+    
+    area = 2*pi*radio*(radio+altura);
+    volumen = pi*radio*radio*altura; 
+    cout<<"El area superficial del cilindro es: "<<area<<endl; 
+    cout<<"El volumen del cilindro es: "<<volumen; 
+    
+    
+    getch(); 
+
+    return 0;
+}
